b8636d64e5c98fcfc1f2b7e8c2dbc658aa3e7137
endavis <endavis@a6199f70-db43-11dd-90d0-c9c6575c7768>
2011-02-20 02:23:11 +0000

    create v6 branch

---
+++ branches/v6/lua/aardutils.lua
@@ -0,0 +1,255 @@
+-- $Id$
+--[[
+http://code.google.com/p/bastmush
+ - Documentation and examples
+
+functions in this module
+removetag - usage:removetag(stylerun)
+  remove the tag in a style run
+
+printstyles - usage:printstyles(stylerun)
+  use colourtell to print a style run
+
+findkeyword - usage: findkeyword(item)
+  return the first keyword of the item that is not an article
+
+getactuallevel - usage: getactuallevel(level, remorts, tier)
+  - find the actual level based on remorts, tier, and current level
+
+convertlevel - usage: convertlevel(level)
+  - opposite of getactuallevel, return a table with keys tier, remort, level
+
+classabb table
+  - a table of class abbreviations, key is the abbreviation, value is
+    the long name for the class
+
+objecttypes table
+  - table of object types
+
+wearlocs table
+  - table of wear locations
+--]]
+
+function hasmore(text)
+  _place = string.find(text, '}')
+  _stlen = string.len(text)
+  if _place == _stlen then
+    return false
+  else
+    return true
+  end
+end
+
+function removetag(styles)
+  if styles[1] then
+    if not hasmore(styles[1].text) then
+      table.remove (styles, 1)  -- get rid of tag
+    else
+      ttext = styles[1].text
+      _place = string.find(ttext, '}')
+      if _place then
+        styles[1].text = string.sub(ttext, _place + 1)
+      end
+    end
+  end
+  return styles
+end
+
+function findkeyword(item)
+  wlist = utils.split(item, " ")
+  badwords = {
+    ring = 1,
+    aardwolf = 1,
+    of = 1,
+    the = 1,
+    davinci = 1,
+    a = 1,
+    an = 1,
+  }
+  local name = ""
+  for i,v in ipairs(wlist) do
+    tfind = string.find(v, "'")
+    if badwords[string.lower(v)] ~= 1 and tfind == nil then
+      name = v
+      break
+    end
+  end
+  return string.lower(name)
+end
+
+function getactuallevel(level, remorts, tier)
+  if level == nil then
+    return -1
+  end
+  tier = tier or 0
+  return (tier * 7 * 201) + (remorts - 1) * 201 + level
+end
+
+function convertlevel(level)
+  level = tonumber(level)
+  if level < 1 then
+    return {tier = -1, remort = -1, level = -1}
+  end
+  if level % (7 * 201) == 0 then
+    tier = math.floor(level / (7 * 201)) - 1
+  else
+    tier = math.floor(level / (7 * 201))
+  end
+  remort = math.floor((level - (tier * 7 * 201)) / 202) + 1
+  if level % 201 == 0 then
+    alevel = 201
+  else
+    alevel = level % 201
+  end
+  return {tier = tier, remort = remort, level = alevel}
+end
+
+function printstyles(styles)
+  if styles then
+    for _, v in ipairs (styles) do
+      if next(v) then
+        for _, v2 in ipairs (v) do
+            ColourTell (RGBColourToName (v2.textcolour),
+                    RGBColourToName (v2.backcolour),
+                    v2.text)
+        end
+        Note("")
+      end
+    end -- for each style run
+    Note ("")  -- wrap up line
+
+  end
+end
+
+classabb = {
+  mag = 'mage',
+  thi = 'thief',
+  pal = 'paladin',
+  war = 'warrior',
+  psi = 'psionicist',
+  cle = 'cleric',
+  ran = 'ranger',
+}
+
+objecttypes = {
+  'Light',
+  'Scroll',
+  'Wand',
+  'Staff',
+  'Weapon',
+  'Treasure',
+  'Armor',
+  'Potion',
+  'Furniture',
+  'Trash',
+  'Container',
+  'Drink',
+  'Key',
+  'Food',
+  'Boat',
+  'Mobcorpse',
+  'Corpse',
+  'Fountain',
+  'Pill',
+  'Portal',
+  'Beacon',
+  'Giftcard',
+  'Gold',
+  'Raw material',
+}
+
+wearlocs = {
+ 'light',
+ 'head',
+ 'eyes',
+ 'lear',
+ 'rear',
+ 'neck1',
+ 'neck2',
+ 'back',
+ 'medal1',
+ 'medal2',
+ 'medal3',
+ 'medal4',
+ 'torso',
+ 'body',
+ 'waist',
+ 'arms',
+ 'lwrist',
+ 'rwrist',
+ 'hands',
+ 'lfinger',
+ 'rfinger',
+ 'legs',
+ 'feet',
+ 'shield',
+ 'wielded',
+ 'second',
+ 'hold',
+ 'float',
+ 'tattoo1',
+ 'tattoo2',
+ 'above',
+ 'portal',
+ 'sleeping',
+}
+
+wearlocreverse = {
+ light = 0,
+ head = 1,
+ eyes = 2,
+ lear = 3,
+ rear = 4,
+ neck1 = 5,
+ neck2 = 6,
+ back = 7,
+ medal1 = 8,
+ medal2 = 9,
+ medal3 = 10,
+ medal4 = 11,
+ torso = 12,
+ body = 13,
+ waist = 14,
+ arms = 15,
+ lwrist = 16,
+ rwrist = 17,
+ hands = 18,
+ lfinger = 19,
+ rfinger = 20,
+ legs = 21,
+ feet = 22,
+ shield = 23,
+ wielded = 24,
+ second = 25,
+ hold = 26,
+ float = 27,
+ tattoo1 = 28,
+ tattoo2 = 29,
+ above = 30,
+ portal = 31,
+ sleeping = 32,
+}
+ 
+ statestrings = {
+  [1] = 'login',
+  [2] = 'motd',
+  [3] = 'active',
+  [4] = 'afk',
+  [5] = 'note',
+  [6] = 'edit',
+  [7] = 'page',
+  [8] = 'combat',
+  [9] = 'sleeping',
+  [11] = 'resting',
+  [12] = 'running'
+}
+
+spelltarget_table = {}
+spelltarget_table[0] = 'special'
+spelltarget_table[1] = 'attack'
+spelltarget_table[2] = 'spellup'
+spelltarget_table[3] = 'selfonly'
+spelltarget_table[4] = 'object'
+
+spelltype_table = {'spell', 'skill'}
+
---
+++ branches/v6/lua/stringfuncs.lua
@@ -0,0 +1,67 @@
+-- $Id$
+--[[
+http://code.google.com/p/bastmush
+ - Documentation and examples
+ 
+functions in this module
+
+wrap - usage: wrap(line, length)
+  wrap the line at length, will try to find closest comma or space
+
+strjoin - usage: strjoin(delimiter, list)
+  join the list with delimiter
+--]]
+
+function wrap(line, length)
+  local lines = {}
+  while #line > length do
+    -- find a space not followed by a space, or a , closest to the end of the line
+    local col = string.find (line:sub (1, length), "[%s,][^%s,]*$")
+    if col and col > 2 then
+--      col = col - 1  -- use the space to indent
+    else
+      col = length  -- just cut off at wrap_column
+    end -- if
+
+    table.insert(lines, line:sub (1, col))
+    line = line:sub (col + 1)
+
+  end
+  table.insert(lines, line)
+  return lines
+end
+
+function strjoin(delimiter, list)
+  local len = #list
+  if len == 0 then 
+    return "" 
+  end
+  local string = list[1]
+  for i = 2, len do 
+    string = string .. delimiter .. list[i] 
+  end
+  return string
+end
+
+function capitalize (s)
+  return string.sub (s, 1, 1):upper () .. string.sub (s, 2):lower ()
+end -- capitalize
+
+function ReadableNumber(num, places)
+    local ret
+    local placeValue = ("%%.%df"):format(places or 0)
+    if not num then
+        return 0
+    elseif num >= 1000000000000 then
+        ret = placeValue:format(num / 1000000000000) .. " T" -- trillion
+    elseif num >= 1000000000 then
+        ret = placeValue:format(num / 1000000000) .. " B" -- billion
+    elseif num >= 1000000 then
+        ret = placeValue:format(num / 1000000) .. " M" -- million
+    elseif num >= 1000 then
+        ret = placeValue:format(num / 1000) .. " K" -- thousand
+    else
+        ret = num -- hundreds
+    end
+    return ret
+end
